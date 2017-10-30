73bcbc087bc3c1dbb9c35ab5611b2c110b027ff9
harrison-symes <symeshjb@gmail.com>
2017-10-30 15:36:46 +1300

    click for more info

--- client/components/PokemonPreview.jsx
+++ client/components/PokemonPreview.jsx
@@ -1,8 +1,38 @@
 import React from 'react'
 
-export default function PokemonPreview ({pokemon}) {
-  return <div className="box column is-4">
-    <img className="media image" src={pokemon.image_url} />
-    <p className="subtitle is-3">#{pokemon.dex_number} - {pokemon.name}</p>
-  </div>
+export default class PokemonPreview extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      isClicked: false
+    }
+    this.click = this.click.bind(this)
+    this.unClick = this.unClick.bind(this)
+  }
+  click() {
+    this.setState({isClicked: true})
+  }
+  unClick() {
+    this.setState({isClicked: false})
+  }
+  render() {
+    const {pokemon} = this.props
+    const {isClicked} = this.state
+    const size = isClicked ? 'is-12' : 'is-4'
+    return isClicked
+      ? <div className='box'>
+        <div className="level">
+          <p className="level-item title is-2">#{pokemon.dex_number} - {pokemon.name}</p>
+        </div>
+        <div className="columns">
+          <img onClick={this.click} src={pokemon.image_url} />
+          <p className="subtitle is-2 is-right">{pokemon.description}</p>
+        </div>
+        <button className="button is-outline" onClick={this.unClick}>Show Less</button>
+      </div>
+      : <div className={`box column is-4`}>
+        <img onClick={this.click} className="media image" src={pokemon.image_url} />
+        <p className="subtitle is-3">#{pokemon.dex_number} - {pokemon.name}</p>
+      </div>
+  }
 }
