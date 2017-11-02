01ca00988c58dc89704a8b7485c5c65b5fa270d5
harrison-symes <symeshjb@gmail.com>
2017-11-02 15:35:02 +1300

    refactor evolutions and can redirect to a pokemon in an evolution chain

--- client/components/Evolutions.jsx
+++ client/components/Evolutions.jsx
@@ -2,34 +2,28 @@ import React from 'react'
 import {connect} from 'react-redux'
 import PokemonSprite from './pokemonSprite'
 
-function Evolutions ({pokemon, pokemonList}) {
+
+function Evolutions ({pokemon, pokemonList, search}) {
   const {evolvesFrom, evolvesInto} = pokemon
+  const DisplayName = ({name}) =>  <a className="subtitle is-4">{name}</a>
+  const DisplayStage = ({stage, isCurrent}) => <p className="subtitle is-6">{isCurrent ? "Current " : ""} Stage {stage}</p>
+
+  const DisplayChainItem = ({pokemon, isCurrent}) => <div onClick={() => search(pokemon.name)}>
+    <DisplayStage stage={pokemon.stage} isCurrent={isCurrent} />
+    <PokemonSprite name={pokemon.name} />
+    <DisplayName name={pokemon.name} />
+  </div>
+
+  const nextStage = pokemonList.find(p => p.name==pokemon.evolvesInto)
+  const prevStage = pokemonList.find(p => p.name==pokemon.evolvesFrom)
+
+  console.log({nextStage, prevStage});
   return <div className="columns has-text-centered">
-    {pokemon.stage == 3 && <div>
-      <p className="subtitle is-6">Stage 1</p>
-      <PokemonSprite name={pokemonList.find(p => p.name==pokemon.evolvesFrom).evolvesFrom} />
-      <p className="subtitle is-4">{pokemonList.find(p => p.name==pokemon.evolvesFrom).evolvesFrom}</p>
-    </div>}
-    {pokemon.evolvesFrom && <div className="">
-      <p className="subtitle is-6">Stage {pokemon.stage -1}</p>
-      <PokemonSprite name={evolvesFrom} />
-      <p className="subtitle is-4">{evolvesFrom}</p>
-    </div>}
-    <div className="">
-      <p className="subtitle is-6">Current Stage ({pokemon.stage})</p>
-      <PokemonSprite name={pokemon.name} />
-      <p className="subtitle is-4">{pokemon.name}</p>
-    </div>
-    {pokemon.evolvesInto && <div className="">
-      <p className="subtitle is-6">Stage {pokemon.stage + 1}</p>
-      <PokemonSprite name={evolvesInto} />
-      <p className="subtitle is-4">{evolvesInto}</p>
-    </div>}
-    {pokemon.stage == 1 && pokemon.stages == 3 && <div>
-      <p className="subtitle is-6">Stage 3</p>
-      <PokemonSprite name={pokemonList.find(p => p.name==pokemon.evolvesInto).evolvesInto} />
-      <p className="subtitle is-4">{pokemonList.find(p => p.name==pokemon.evolvesInto).evolvesInto}</p>
-    </div>}
+    {pokemon.stage == 3 && <DisplayChainItem pokemon={pokemonList.find(p => p.name==prevStage.evolvesFrom)} />}
+    {pokemon.evolvesFrom && <DisplayChainItem pokemon={prevStage} />}
+    <DisplayChainItem pokemon={pokemon} isCurrent={true} />
+    {pokemon.evolvesInto && <DisplayChainItem pokemon={nextStage} />}
+    {pokemon.stage == 1 && pokemon.stages == 3 && <DisplayChainItem pokemon={pokemonList.find(p => p.name==nextStage.evolvesInto)} />}
   </div>
 }
 
@@ -39,4 +33,10 @@ const mapStateToProps = ({pokemon}) => {
   }
 }
 
-export default connect(mapStateToProps)(Evolutions)
+const mapDispatchToProps = (dispatch) => {
+  return {
+    search: (search) => dispatch({type: 'UPDATE_SEARCH', search})
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Evolutions)
--- client/reducers/search.js
+++ client/reducers/search.js
@@ -2,7 +2,7 @@ export default function searchReducer (state = '', action) {
   switch(action.type) {
     case 'UPDATE_SEARCH':
       return action.search
-    case 'RESET':
+    case 'RESET_SEARCH':
       return ''
     default:
       return state
