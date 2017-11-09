221e73247a154d4b1b1054b03aaf1cede5c565aa
harrison-symes <symeshjb@gmail.com>
2017-11-10 12:30:46 +1300

    advanced options polished

--- client/components/SearchBar.jsx
+++ client/components/SearchBar.jsx
@@ -39,43 +39,44 @@ class SearchBar extends Component {
         {searchedTypes.map(type => <button onClick={() => this.props.searchType(type)} style={{backgroundColor: solveColor(type)}} className={`button column is-2 has-text-centered`}>{type}</button>)}
         <button onClick={this.reset} className="button is-warning">Reset</button>
       </div>
-      {showAdvanced && <div>
-        <div className="container">
-          <select onChange={(e) => searchTier(e.target.value)} className="input has-text-centered">
-            <option value={null} >All tiers</option>
-            <option value={"LC"} >LC: Little Cup</option>
-            <option value={"PU"} >PU: Stink Tier</option>
-            <option value={"BL4"} >BL4: BorderLine (PU - NU)</option>
-            <option value={"NU"} >NU: Never Used</option>
-            <option value={"BL3"} >BL3: BorderLine (NU - RU)</option>
-            <option value={"RU"} >RU: Rarely Used</option>
-            <option value={"BL2"} >BL2: BorderLine (RU - UU)</option>
-            <option value={"UU"} >UU: Under Used</option>
-            <option value={"BL"} >BL: BorderLine (UU - OU)</option>
-            <option value={"OU"} >OU: Over Used</option>
-            <option value={"Uber"} >Uber: The Nuts</option>
-          </select>
-        </div>
-        <div className="container">
-          <select defautlValue={searchGen} onChange={(e) => searchForGen(e.target.value)} className="input has-text-centered">
-            <option value={null} >All Generations</option>
-            <option value={"RB"} >RBGY (1): Red / Blue / Green / Yellow </option>
-            <option value={"GS"} >GSC (2): Gold / Silver / Crystal</option>
-            <option value={"RS"} >RSE (3): Ruby / Sapphire / Emerald </option>
-            <option value={"DP"} >DPP (4): Diamond / Pearl / Platinum </option>
-            <option value={"BW"} >BW (5): Black / White (1/2)</option>
-            <option value={"XY"} >XY (6): X / Y</option>
-            <option value={"SM"} >SM (7): Sun / Moon</option>
-          </select>
-        </div>
-        <div className="section">
-          <div className="container columns is-multiline">
-            {typeList.map(type => <p onClick={() => searchType(type)} style={{backgroundColor: solveColor(type) }} className={`button column is-2 has-text-centered`}>{type}</p>)}
+      {showAdvanced && <div className="section">
+        <div className="level">
+
+          <div className="container">
+            <select onChange={(e) => searchTier(e.target.value)} className="input has-text-centered">
+              <option value={null} >All tiers</option>
+              <option value={"LC"} >LC: Little Cup</option>
+              <option value={"PU"} >PU: Stink Tier</option>
+              <option value={"BL4"} >BL4: BorderLine (PU - NU)</option>
+              <option value={"NU"} >NU: Never Used</option>
+              <option value={"BL3"} >BL3: BorderLine (NU - RU)</option>
+              <option value={"RU"} >RU: Rarely Used</option>
+              <option value={"BL2"} >BL2: BorderLine (RU - UU)</option>
+              <option value={"UU"} >UU: Under Used</option>
+              <option value={"BL"} >BL: BorderLine (UU - OU)</option>
+              <option value={"OU"} >OU: Over Used</option>
+              <option value={"Uber"} >Uber: The Nuts</option>
+            </select>
+          </div>
+          <div className="container">
+            <select defautlValue={searchGen} onChange={(e) => searchForGen(e.target.value)} className="input has-text-centered">
+              <option value={null} >All Generations</option>
+              <option value={"RB"} >RBGY (1): Red / Blue / Green / Yellow </option>
+              <option value={"GS"} >GSC (2): Gold / Silver / Crystal</option>
+              <option value={"RS"} >RSE (3): Ruby / Sapphire / Emerald </option>
+              <option value={"DP"} >DPP (4): Diamond / Pearl / Platinum </option>
+              <option value={"BW"} >BW (5): Black / White (1/2)</option>
+              <option value={"XY"} >XY (6): X / Y</option>
+              <option value={"SM"} >SM (7): Sun / Moon</option>
+            </select>
           </div>
         </div>
+        <div className=" columns is-multiline">
+          {typeList.map(type => <p onClick={() => searchType(type)} style={{backgroundColor: solveColor(type) }} className={`button column is-2 has-text-centered`}>{type}</p>)}
+        </div>
       </div>}
+
       <button className="button is-info" onClick={this.toggleAdvanced}>{showAdvanced ? "Hide Advanced" : "Show Advanced Options"}</button>
-      <br />
     </div>
   }
 }
